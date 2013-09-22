4a2cebf8eddbff984f085aaac1dc9b80339dcaa5
Steven Rojas <stvrjs.k12@gmail.com>
2013-09-21 19:42:26 -0600

    Adding classes

---
+++ prjAdminSCHUNI/src/pkgClases/clsCoordinador.java
@@ -0,0 +1,24 @@
+/**
+ * Classes Package
+ */
+package pkgClases;
+
+/**
+ * @author      Steven Rojas <stvrjs.k12@gmail.com>
+ * @version     1.0                 (current version number of program)
+ * @since       2013-09-21          (the version of the package this class was first added to)
+ */
+public class clsCoordinador extends clsUser {
+
+	/**
+	 * Coordinator class constructor
+	 * 
+	 * @param pID
+	 * @param pName
+	 * @param pPassword
+	 */
+	public clsCoordinador(String pID, String pName, String pPassword){
+		super(pID, pName, pPassword);
+	}
+
+}
